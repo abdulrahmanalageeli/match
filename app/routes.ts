ef4a029f01022c25647875744e6943af8a596367
Abdulrahman Aloqayli <219110612@psu.edu.sa>
2025-09-20 13:47:12 +0300

    Fixed spacing

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/welcome.tsx"),         // maps to "/" - welcome is now default
+  route("welcome", "routes/welcome.tsx"), // keep /welcome route working
   route("home", "routes/home.tsx"),    // moved home to /home
   route("admin", "routes/admin.tsx"),
   route("matrix", "routes/matrix.tsx"),

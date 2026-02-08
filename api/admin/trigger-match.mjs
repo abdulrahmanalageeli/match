1aed1de318f81322395461a42f977dbc5e41b094
Abdulrahman Aloqayli <219110612@psu.edu.sa>
2026-02-08 13:29:32 +0300

    Initial push: Groups page redesign + animations

--- api/admin/trigger-match.mjs
+++ api/admin/trigger-match.mjs
@@ -1907,10 +1907,7 @@ async function generateGroupMatches(participants, match_id, eventId, options = {
       else if (targetSize === 5) score -= 5
 
       // Single-female penalty (size 4 only)
-      if (hasSingleFemale) {
-       // console.log(?? Skipping group combination [] - single female in group of 4 (M, F) - HARD CONSTRAINT)
-        continue
-      }
+
 
       return score
     }

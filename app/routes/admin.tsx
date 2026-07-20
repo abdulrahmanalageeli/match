14df33c4321a4fcff60c344c0d3f303879398ef0
Abdulrahman Aloqayli <219110612@psu.edu.sa>
2026-07-20 15:41:12 +0300

    Initial push: SAME AND OPPOSITE

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1041,7 +1041,7 @@ https://match-omega.vercel.app/welcome?token=${secureToken}
 إذا لم تتمكنوا من الحضور، يرجى إبلاغنا فوراً حتى نعطي الفرصة لمشارك آخر.
 
 📋 *تفاصيل الفعالية كاملة:*
-https://meetu.ps/e/Q8Qpd/Lh7Kd/i
+https://meetu.ps/e/Q9zQM/Lh7Kd/i
 
 📍 *تفاصيل الفعالية:*
 المكان: ${d.locationName}
